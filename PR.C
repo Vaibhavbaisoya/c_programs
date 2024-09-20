c1655e71c790759ed587879b64594a051f9c9d8f
Vaibhavbaisoya <143985894+Vaibhavbaisoya@users.noreply.github.com>
2024-09-20 12:18:26 +0530

    Add files via upload

---
+++ PR.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int a=10,b=20;
+    clrscr();
+    printf("area of square: %d\n",a*a);
+    printf("area of rectangle: %d\n",a*b);
+    printf("area of trinangle:%d\n",a*a*a);
+    printf("area of circle :%d\n",2*3.14*a);
+    getch();
+}
